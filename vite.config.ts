f270f005baeb0e322e519f745b8a380b423c4e1e
GodFury <119965412+Vedansh564@users.noreply.github.com>
2026-03-18 22:10:32 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'New_ML_Diagnosis_System',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
